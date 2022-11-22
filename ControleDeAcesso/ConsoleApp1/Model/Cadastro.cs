1bad6d24a5a0ec3f9dc8e254c4af8776edd428a7
Guilherme Pontes <83968921+GuihPontes@users.noreply.github.com>
2022-11-22 09:16:57 -0300

    Add files via upload
    atividade controle de acesso

---
+++ ControleDeAcesso/ConsoleApp1/Model/Cadastro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Model
+{
+    public class Cadastro 
+    {
+        public Cadastro(List<Usuario> usuarios, List<Ambiente> ambientes)
+        {
+            usuarios = new List<Usuario> { };
+            ambientes= new List<Ambiente> { };  
+        }
+
+        public List<Usuario> Usuarios { get; set; }
+        public List<Ambiente> Ambientes { get; set; }
+        public void CriarUsuario(Usuario usuario)
+        {
+         Usuarios.Add(usuario);
+        }
+
+        public void Remover(Usuario usuario)
+        {
+            Usuarios.Remove(usuario);   
+        }
+    }
+}
